190065fd1fc81bf35775e0744c57d5b8e056b5ec
andy <morrison.andrew422@gmail.com>
2023-03-02 16:14:36 -0500

    finish clock

--- view/callback.js
+++ view/callback.js
@@ -1,8 +1,8 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
-    // const date = new Date();
-    const date = new Date('December 17, 1995 03:58:00');
+    const date = new Date();
+    // const date = new Date('December 17, 1995 23:55:50');
     // 2. Store the hours, minutes, and seconds.
     this.hours = date.getHours();
     this.minutes = date.getMinutes();
@@ -12,12 +12,33 @@ class Clock {
     // 4. Schedule the tick at 1 second intervals.
     setInterval(() => {
       this._tick();
-    }, 50);
+    }, 1000);
   };
 
   printTime() {
     // Format the time in HH:MM:SS
-    let string = `${this.hours}:${this.minutes}:${this.seconds}`;
+    // let secondsSpaceHolder = ``
+    // this.seconds < 10 ? secondsSpaceHolder = `0` : ``
+    // let minutesSpaceHolder
+    // this.minutes < 10 ? minutesSpaceHolder = `0` : ``
+    // let hoursSpaceHolder
+    // this.hours < 10 ? hoursSpaceHolder = `0` : ``
+
+    let stringSeconds = this.seconds.toString();
+    if (stringSeconds < 10) {
+      stringSeconds = `0` + stringSeconds
+    }
+    let stringMinutes = this.minutes.toString();
+    if (stringMinutes < 10) {
+      stringMinutes = `0` + stringMinutes
+    }
+    let stringHours = this.hours.toString();
+    if (stringHours < 10) {
+      stringHours = `0` + stringHours
+    }
+
+    let string = `${stringHours}:${stringMinutes}:${stringSeconds}`;
+    // let string = `${this.hours}:${this.minutes}:${this.seconds}`;
     // Use console.log to print it.
     console.log(string);
   }
@@ -32,9 +53,8 @@ class Clock {
       this.minutes = 0;
       ++this.hours;
     }
-    if (this.hours === 23) {
-      this.minutes = 0;
-      ++this.hours;
+    if (this.hours === 24) {
+      this.hours = 0;
     }
     
     ++this.seconds
